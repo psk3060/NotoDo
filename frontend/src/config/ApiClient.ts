e9f94662af2c30b83b3ec2596e454f745932f07c
psk <psk3060@naver.com>
2026-02-15 17:36:06 +0900

    API 설정파일 완료

--- frontend/src/config/ApiClient.ts
+++ frontend/src/config/ApiClient.ts
@@ -1,16 +1,46 @@
-import axios from "axios";
+import axios, { AxiosInstance, InternalAxiosRequestConfig } from "axios";
 
-import localAuthStore from '@/store/authStore';
 
-export const apiClient = axios.create(
+import {authStore} from "@/features/auth/stores/authStore";
+import { ENV } from "@/config/env";
+
+export const apiClient : AxiosInstance = axios.create(
     {
-        baseURL : 'http://localhost:8000'
+        baseURL : ENV.API_BASE_URL
         , withCredentials: true
+        , headers : {
+            'Content-Type' : 'application/json'
+        }
     }
 )
 
-apiClient.interceptors.request.use(config => {
-    const userId = localAuthStore.getState().userId; // 항상 최신 값
-    if(userId) config.headers!['userId'] = userId;
+/**
+ * Request Interceptor : 크로스체크 userId를 항상 Header에 추가
+ */
+apiClient.interceptors.request.use((config : InternalAxiosRequestConfig) => {
+    const userId = authStore.getState().userId;
+
+    if(userId && config.headers) {
+        config.headers!['userId'] = userId;
+    }
     return config;
+}, (error) => {
+    return Promise.reject(error);
 });
+
+
+apiClient.interceptors.response.use(
+    (response) => response
+    , (error) => {
+        if(error.response?.data) {
+            return Promise.reject(error);
+        }
+
+        return Promise.reject({
+            data : {
+                code : 'network_error',
+                message : '네트워크 오류가 발생했습니다.'
+            }
+        });
+    }
+);
